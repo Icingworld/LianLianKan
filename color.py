45efa6ce98e585750451bc7e1e28f6aa4bf03989
Icingworld <51267053+Icingworld@users.noreply.github.com>
2021-07-13 21:54:43 +0800

    Create color.py

---
+++ color.py
@@ -0,0 +1,32 @@
+import random
+
+
+def rand():
+    a = random.randint(20, 240)
+    return a
+
+
+# 生成随机颜色列表，思路同check.py
+def color(color_list, num_):
+    num = 0
+    if len(color_list) == 0:
+        new = [num_, (rand(), rand(), rand())]
+        color_list.append(new)
+    else:
+        for colors in color_list:
+            if num_ == colors[0]:
+                return colors[1]
+            else:
+                if num == len(color_list) - 1:
+                    new = [num_, (rand(), rand(), rand())]
+                    color_list.append(new)
+                else:
+                    num += 1
+    return color_list
+
+
+# 通过num获取颜色
+def get_color(color_list, num):
+    for colors in color_list:
+        if num == colors[0]:
+            return colors[1]
